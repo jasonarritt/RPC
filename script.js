3be7a3b142b3e03208625133763a59ff9026df4d
Jason Arritt <jsn.arritt@gmail.com>
2023-09-25 18:48:14 -0400

    Added score and messsages

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const message = document.querySelector('.message');
 const score = document.querySelector('.score');
 const buttons = document.querySelectorAll('button');
+let winner = [0, 0];
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', playGame);
@@ -20,38 +21,52 @@ function playGame(event) {
   console.log(playerSelection);
   console.log(computerSelection);
   let result = checkWinner(playerSelection, computerSelection);
+
+  console.log(result);
+
+  if (result == 'Player') {
+    result += ' Wins!';
+    winner[0]++;
+  } else if (result == 'Computer') {
+    result += ' Wins.';
+    winner[1]++;
+  } else {
+    result = 'There was a Draw. No winner.';
+  }
+  score.innerHTML = 'Player 1 [' + winner[0] + '] Player 2 [' + winner[1] + ']';
+  messenger(
+    playerSelection + ' vs ' + computerSelection + '<br>' + result + '</br>'
+  );
+  console.log(result);
 }
 
 function checkWinner(player, computer) {
   if (player === computer) {
-    console.log('D');
     return 'Draw';
   }
   if (player === 'ROCK') {
     if (computer === 'PAPER') {
-      console.log('C');
       return 'Computer';
     } else {
-      console.log('P');
       return 'Player';
     }
   }
   if (player === 'PAPER') {
     if (computer === 'SCISSORS') {
-      console.log('C');
       return 'Computer';
     } else {
-      console.log('P');
       return 'Player';
     }
   }
   if (player === 'SCISSORS') {
     if (computer === 'ROCK') {
-      console.log('C');
       return 'Computer';
     } else {
-      console.log('P');
       return 'Player';
     }
   }
+}
+
+function messenger(mes) {
+  message.innerHTML = mes;
 }
